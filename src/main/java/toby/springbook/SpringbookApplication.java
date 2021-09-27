e42825714684f98d448cd3868ff346802f2f0b89
Song Inbong <sinb57@naver.com>
2021-09-27 15:32:02 +0900

    1.3 DAO의 확장 - 클래스의 분리
    DB 커넥션 생성 기능을 독립시킨 SimpleConnectionMaker 클래스 생성.

--- src/main/java/toby/springbook/SpringbookApplication.java
+++ src/main/java/toby/springbook/SpringbookApplication.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class SpringbookApplication {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new NUserDao();
+		UserDao dao = new UserDao();
 
 		User user = new User();
 		user.setId("whiteship");
