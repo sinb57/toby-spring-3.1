ebcc298fd8bb834f30931f3be628d8fcb80a6dfc
Song Inbong <sinb57@naver.com>
2021-09-27 16:03:10 +0900

    테스트 코드 분리

--- src/main/java/toby/springbook/SpringbookApplication.java
+++ src/main/java/toby/springbook/SpringbookApplication.java
@@ -1,37 +1,12 @@
 package toby.springbook;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import toby.springbook.user.dao.DConnectionMaker;
-import toby.springbook.user.dao.UserDao;
-import toby.springbook.user.domain.User;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class SpringbookApplication {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao(new DConnectionMaker());
-
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
-
-		dao.add(user);
-
-		System.out.println(user.getId() + " 등록 성공");
-
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-
-		System.out.println(user2.getId() + " 조회 성공");
-	}
-
-	/*
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbookApplication.class, args);
 	}
-	*/
 }
---
+++ src/test/java/toby/springbook/UserDaoTest.java
@@ -0,0 +1,29 @@
+package toby.springbook;
+
+import toby.springbook.user.dao.DConnectionMaker;
+import toby.springbook.user.dao.UserDao;
+import toby.springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao(new DConnectionMaker());
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
