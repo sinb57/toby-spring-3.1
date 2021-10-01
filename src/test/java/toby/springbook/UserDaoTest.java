ccc410cb37e0307ca5f448141b8f580e2603f49f
Song Inbong <sinb57@naver.com>
2021-10-01 16:59:19 +0900

    2.4 DI와 테스트 - 테스트 코드를 위한 별도의 DI 설정
    테스트를 위한 별도의 applicationContext.xml 생성.

--- src/test/java/toby/springbook/UserDaoTest.java
+++ src/test/java/toby/springbook/UserDaoTest.java
@@ -8,30 +8,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import toby.springbook.user.dao.UserDao;
 import toby.springbook.user.domain.User;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations="/applicationContext.xml")
-@DirtiesContext
 public class UserDaoTest {
+
     @Autowired
+    private ApplicationContext context;
     private UserDao dao;
 
     @BeforeEach
     public void setUp() {
-        DataSource dataSource = new SingleConnectionDataSource(
-                "jdbc:mysql://localhost/testdb", "root", "qwe123!@#", true);
-        dao.setDataSource(dataSource);
+        dao = context.getBean("userDao", UserDao.class);
     }
 
     @Test
