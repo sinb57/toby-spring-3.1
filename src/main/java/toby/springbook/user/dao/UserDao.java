f601f10d0715122b81d39b38f7ef14bb2a3bf9b5
Song Inbong <sinb57@naver.com>
2021-10-03 13:56:34 +0900

    3.3 전략과 클라이언트의 동거 - 로컬 클래스
    UserDao의 add() 메소드와 마찬가지로
    deleteAll() 메소드 또한 deleteStatement 클래스를
    익명 내부 클래스로 전환.

--- src/main/java/toby/springbook/user/dao/UserDao.java
+++ src/main/java/toby/springbook/user/dao/UserDao.java
@@ -13,19 +13,19 @@ public class UserDao {
     public void add(final User user) throws SQLException {
 
         jdbcContextWithStatementStrategy(
-            new StatementStrategy() {
-                @Override
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement(
-                            "insert into users(id, name, password) values(?,?,?)");
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement(
+                                "insert into users(id, name, password) values(?,?,?)");
 
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
 
-                    return ps;
+                        return ps;
+                    }
                 }
-            }
         );
 
     }
@@ -59,8 +59,15 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        jdbcContextWithStatementStrategy(
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement("delete from users");
+                        return ps;
+                    }
+                }
+        );
     }
 
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
