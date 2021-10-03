564591270254ca192275bfc103547ffa273ceae8
Song Inbong <sinb57@naver.com>
2021-10-03 15:22:45 +0900

    3.5 템플릿/콜백의 응용
    Calculator 클래스에 템플릿/콜백 적용

---
+++ src/main/java/toby/springbook/learning/BufferedReaderCallback.java
@@ -0,0 +1,10 @@
+package toby.springbook.learning;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface BufferedReaderCallback {
+    Integer doSomethingWithReader(BufferedReader br) throws IOException;
+}
+
+
