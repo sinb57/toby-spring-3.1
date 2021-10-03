d5a479f9dda1dcceb66a8ab7757fe127540baf3b
Song Inbong <sinb57@naver.com>
2021-10-03 17:21:53 +0900

    3.6 스프링의 JdbcTemplate - RowMapper 중복 제거
    get(), getAll()에서 사용된 RowMapper 람다식을
    익명 내부 클래스로 선언하여 인스턴스 변수로 사용.

--- src/main/java/toby/springbook/user/dao/UserDao.java
+++ src/main/java/toby/springbook/user/dao/UserDao.java
@@ -1,45 +1,49 @@
 package toby.springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import toby.springbook.user.domain.User;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
 
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new User(
+                    rs.getString("id"),
+                    rs.getString("name"),
+                    rs.getString("password")
+            );
+        }
+    };
+
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public void add(final User user) {
-        jdbcTemplate.update("insert into users values (?,?,?)", user.getId(), user.getName(), user.getPassword());
+        jdbcTemplate.update("insert into users values (?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) {
-        return jdbcTemplate.queryForObject("select  * from users where id = ?",
-                (rs, nowNum) -> new User(
-                        rs.getString("id"),
-                        rs.getString("name"),
-                        rs.getString("password")
-                ), id);
+        return jdbcTemplate.queryForObject("select  * from users where id = ?", userMapper, id);
     }
 
     public List<User> getAll() {
-        return jdbcTemplate.query("select * from users",
-                (rs, nowNum) -> new User(
-                        rs.getString("id"),
-                        rs.getString("name"),
-                        rs.getString("password")
-                ));
+        return jdbcTemplate.query("select * from users", userMapper);
     }
 
     public void deleteAll() {
         jdbcTemplate.update("delete from users");
     }
-
 
     public int getCount() {
         return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
