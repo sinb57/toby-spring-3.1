31e5c4e8a9c7212367b91128c2816f97e9d221bc
Song Inbong <sinb57@naver.com>
2021-10-03 16:46:05 +0900

    리팩토링 : StatementStrategy 삭제 및 불필요한 import, 주석 삭제

--- src/main/java/toby/springbook/user/dao/UserDao.java
+++ src/main/java/toby/springbook/user/dao/UserDao.java
@@ -1,20 +1,16 @@
 package toby.springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import toby.springbook.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.*;
 
 public class UserDao {
 
     private JdbcTemplate jdbcTemplate;
-    private DataSource dataSource; // 아직 JdbcContext를 적용하지 않은 메소드를 위해 남겨둔다.
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
     }
 
     public void add(final User user) {
