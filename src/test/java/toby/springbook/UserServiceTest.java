c44eea7eb913392f3d0eab2a2261bea2f8a6cfae
Song Inbong <sinb57@naver.com>
2021-10-07 18:09:49 +0900

    5.1 사용자 레벨 관리 기능 추가 - 사용자 신규 등록 로직

---
+++ src/test/java/toby/springbook/UserServiceTest.java
@@ -0,0 +1,76 @@
+package toby.springbook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import toby.springbook.user.dao.UserDao;
+import toby.springbook.user.domain.Level;
+import toby.springbook.user.domain.User;
+import toby.springbook.user.services.UserService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations="/applicationContext.xml")
+public class UserServiceTest {
+
+    @Autowired UserService userService;
+    @Autowired UserDao userDao;
+    List<User> users;
+
+    @BeforeEach
+    public void setUp() {
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+    }
+
+    @Test
+    public void upgradeLevels() {
+        for(User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
+    }
+
+    @Test
+    public void add() {
+        User userWithLevel = users.get(4);
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel()).isEqualTo(userWithLevel.getLevel());
+        assertThat(userWithoutLevelRead.getLevel()).isEqualTo(Level.BASIC);
+    }
+}
